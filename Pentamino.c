8bfe8985c9cd70c0a81d82ac336a94a33c25ac23
lucadeslot <deslot.luca@gmail.com>
2020-10-13 10:01:31 +0200

    Création des premiers fichiers

---
+++ Pentamino.c
@@ -0,0 +1,5 @@
+//
+// Created by Luca Deslot on 13/10/2020.
+//
+
+#include "Pentamino.h"
---
+++ Pentamino.h
@@ -0,0 +1,8 @@
+//
+// Created by Luca Deslot on 13/10/2020.
+//
+
+#ifndef PROJET_PENTAMINO_H
+#define PROJET_PENTAMINO_H
+
+#endif //PROJET_PENTAMINO_H
