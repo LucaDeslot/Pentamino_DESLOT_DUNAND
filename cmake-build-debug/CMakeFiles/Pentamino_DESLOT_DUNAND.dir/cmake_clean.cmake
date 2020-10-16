0f14596d0d91b0548bd41f3c67af22d2786c0b75
lucadeslot <deslot.luca@gmail.com>
2020-10-16 16:19:08 +0200

    Premier commit

---
+++ cmake-build-debug/CMakeFiles/Pentamino_DESLOT_DUNAND.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pentamino_DESLOT_DUNAND.dir/main.c.o"
+  "Pentamino_DESLOT_DUNAND"
+  "Pentamino_DESLOT_DUNAND.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pentamino_DESLOT_DUNAND.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
