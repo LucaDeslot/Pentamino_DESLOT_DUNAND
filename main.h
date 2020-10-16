3acad6985260e293e309d3e63f595c3986f6eb53
nathand <nathandunand@orange.fr>
2020-10-16 18:29:33 +0200

    Ajout lecture fichier (createPieces non fonctionnel), création de la grille et des pièces (non fonctionnel).

---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by nathand on 16/10/2020.
+//
+
+#ifndef PENTAMINO_DESLOT_DUNAND_MAIN_H
+#define PENTAMINO_DESLOT_DUNAND_MAIN_H
+
+#endif //PENTAMINO_DESLOT_DUNAND_MAIN_H
+
+void createPieces(int* pieces, char* levelNumber);
